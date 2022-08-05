19122b49dcd05f96de9f4ad7b7e6fa08505d1f59
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-08-04 22:28:37 -0400

    style: Update styles for dropdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Logica referente a index.html
 function start(e) {
   if (e.keyCode == 13) {
-      load()
+    load();
   }
 }
 window.onkeypress = start;
@@ -10,29 +10,37 @@ const load = () => {
   // en vez de input ponle inputValue, porque lo q guardas ahí es el valor del input
   // nada... cosas semánticas
   const InitialInputValue = document.getElementById("search-input").value;
-  let inputValue = InitialInputValue.toLowerCase()
+  let inputValue = InitialInputValue.toLowerCase();
   // Esto es lo del redirect
   window.location.replace(`/details.html?name=${inputValue}`);
 };
 
-const input = document.querySelector(`#search-input`)
-const pokemonsList = document.querySelector(`#pokemons`)
+const input = document.querySelector(`#search-input`);
+const pokemonsList = document.querySelector(`#pokemons`);
 
-let pokemonsListNames = []
- 
+let pokemonsListNames = [];
+
 window.onload = () => {
-    fetch(`${BASE_URL}/pokemon?offset=0&limit=12`).then(data => data.json())
-    .then(data => {pokemonsListNames = data.results; renderPokemons(pokemonsListNames)})
-}
+  fetch(`${BASE_URL}/pokemon?offset=0&limit=12`)
+    .then((data) => data.json())
+    .then((data) => {
+      pokemonsListNames = data.results;
+      renderPokemons(pokemonsListNames);
+    });
+};
 
-const createPokeItems = pokemonsListNames => pokemonsListNames.map(pokemon => `<li>${pokemon.name}</li>`)
+const createPokeItems = (pokemonsListNames) =>
+  pokemonsListNames.map((pokemon) => `<li>${pokemon.name}</li>`);
 
-input.addEventListener(`keyup`, e => {
-    const newPpokemons = pokemonsListNames.filter(pokemon => pokemon.name.includes(input.value))
-    renderPokemons(newPpokemons)
-})
+input.addEventListener(`keyup`, (e) => {
+  const newPpokemons = pokemonsListNames.filter((pokemon) =>
+    pokemon.name.includes(input.value)
+  );
+  renderPokemons(newPpokemons);
+});
 
 const renderPokemons = (pokemonsListNames) => {
-    const pokeItems = createPokeItems(pokemonsListNames)
-    console.log(pokeItems)
-}
+  const pokeItems = createPokeItems(pokemonsListNames);
+  pokemonsList.innerHTML = pokeItems.join(" ");
+  console.log(pokeItems);
+};
