4a05af246ab74644475a0506f94810e56fd779fc
Risbel Suarez <risbel961019@gmail.com>
2023-04-13 23:30:55 -0400

    bug-fix: Fixing error in dropdown

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
-// Logica referente a index.html
-function start(e) {
-  if (e.keyCode == 13) {
-    load();
-  }
-}
-window.onkeypress = start;
+let pokemonsListNames = [];
 
-const load = () => {
-  // en vez de input ponle inputValue, porque lo q guardas ahí es el valor del input
-  // nada... cosas semánticas
-  const InitialInputValue = document.getElementById("search-input").value;
-  let inputValue = InitialInputValue.toLowerCase();
-  // Esto es lo del redirect
-  window.location.replace(`/details.html?name=${inputValue}`);
+window.onload = () => {
+  fetch(`${BASE_URL}/pokemon?offset=0&limit=1000`)
+    .then((data) => data.json())
+    .then((data) => {
+      pokemonsListNames = data.results;
+    });
 };
 
 const input = document.querySelector(`#search-input`);
 const pokemonsList = document.querySelector(`#pokemons`);
 
-let pokemonsListNames = [];
-
-window.onload = () => {
-  fetch(`${BASE_URL}/pokemon?offset=0&limit=12`)
-    .then((data) => data.json())
-    .then((data) => {
-      pokemonsListNames = data.results;
-      renderPokemons(pokemonsListNames);
-    });
-};
-
-const createPokeItems = (pokemonsListNames) =>
-  pokemonsListNames.map((pokemon) => `<li>${pokemon.name}</li>`);
-
 input.addEventListener(`keyup`, (e) => {
-  const newPpokemons = pokemonsListNames.filter((pokemon) =>
-    pokemon.name.includes(input.value)
-  );
-  renderPokemons(newPpokemons);
+  const inputValue = input.value.trim().toLowerCase(); // Se utiliza el trim para eliminar los espacios vacíos a los lados del valor de entrada y se convierte a minúsculas para asegurar una búsqueda sin distinción de mayúsculas o minúsculas.
+  const newPpokemons = pokemonsListNames.filter((pokemon) => pokemon.name.includes(inputValue));
+
+  if (inputValue.length > 0) {
+    renderPokemons(newPpokemons);
+  } else {
+    pokemonsList.innerHTML = ""; // Si el valor de entrada es vacío, entonces no se muestra ninguna lista.
+  }
 });
 
 const renderPokemons = (pokemonsListNames) => {
   const pokeItems = createPokeItems(pokemonsListNames);
   pokemonsList.innerHTML = pokeItems.join(" ");
-  console.log(pokeItems);
+};
+
+const createPokeItems = (pokemonsListNames) => pokemonsListNames.map((pokemon) => `<li>${pokemon.name}</li>`);
+
+window.onkeypress = start;
+
+function start(e) {
+  if (e.keyCode == 13) {
+    load();
+  }
+}
+
+const load = () => {
+  const InitialInputValue = document.getElementById("search-input").value;
+  let inputValue = InitialInputValue.toLowerCase();
+  window.location.replace(`/details.html?name=${inputValue}`);
 };
